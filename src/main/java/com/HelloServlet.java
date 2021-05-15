85ccd008b5172783b2c59c967f9beade34cd994c
Anton Lynnyk <linnik.anton13@gmail.com>
2021-05-15 12:10:38 +0300

    First Servlet

---
+++ src/main/java/com/HelloServlet.java
@@ -0,0 +1,33 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="helloservlet", value = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    private String message;
+
+    public void init(){
+        message = "Hello world";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
